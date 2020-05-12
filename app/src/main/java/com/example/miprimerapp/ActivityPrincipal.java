5fada60db6fa2648c8f21348c82711d627218ed6
Adrian1jr <47188608+Adrian1jr@users.noreply.github.com>
2020-05-12 17:12:32 -0500

    Hola

--- app/src/main/java/com/example/miprimerapp/ActivityPrincipal.java
+++ app/src/main/java/com/example/miprimerapp/ActivityPrincipal.java
@@ -2,7 +2,9 @@ package com.example.miprimerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ActivityPrincipal extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class ActivityPrincipal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+    }
+
+    public void Escanear(View view){
+        Intent siguiente = new Intent(this,MainActivity.class);
+        startActivity(siguiente);
     }
 }
